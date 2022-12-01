f75ce6f76eae9ab16a433b50d0eacd686a56d7bf
GarryZaldyDeGuzman <112869392+GarryZaldyDeGuzman@users.noreply.github.com>
2022-12-01 15:26:20 +0800

    Update Books.py

--- Books.py
+++ Books.py
@@ -4,48 +4,55 @@ try:
         def __init__(self):
             pass
             
-        def set_book_id(self):
+            self.setBook_ID()
+            self.setBook_Title()
+            self.setBook_Author()
+            self.setBook_Type()
+            self.setBook_Year()
+            self.setNumber_CopiesAvailable()
+            
+        def setBook_ID(self):
             import random
             self.ID = random.randint(1000,2000)
 
-        def set_book_title(self):
-            self.title = input("Enter the Book Title: ")
+        def setBook_Title(self):
+            self.Title = input("Enter the Book Title: ")
         
-        def set_book_author(self):
-            self.author = input("Enter the Book Author")
+        def setBook_Author(self):
+            self.Author = input("Enter the Book Author")
 
-        def set_book_type(self):
-            self.type = input("Enter the type of Book: ")
+        def setBook_Type(self):
+            self.Type = input("Enter the type of Book: ")
 
-        def set_book_year(self):
-            self.year = int(input("Enter the Book Publication Year: "))
+        def setBook_Year(self):
+            self.Year = int(input("Enter the Book Publication Year: "))
 
-        def set_number_of_copies_available(self):
-            self.copies = int(input("Enter the number of copies available: "))
+        def setNumber_CopiesAvailable(self):
+            self.Copies = int(input("Enter the number of copies available: "))
 
-        def get_book_id(self):
+        def getBook_ID(self):
             ID = self.ID
             return ID
 
-        def get_book_title(self):
-            title = self.title
-            return title
+        def getBook_Title(self):
+            Title = self.Title
+            return Title
 
-        def get_book_author(self):
-            author= self.author
-            return author
+        def getBook_Author(self):
+            Author= self.Author
+            return Author
 
-        def get_book_type(self):
-            type = self.type
-            return type
+        def getBook_Type(self):
+            Type = self.Type
+            return Type
 
-        def get_book_year(self):
-            year = self.year
-            return year
+        def getBook_Year(self):
+            Year = self.Year
+            return Year
 
-        def get_number_of_copies_available(self):
-            copies = self.copies
-            return copies
+        def getNumber_CopiesAvailable(self):
+            Copies = self.Copies
+            return Copies
 
 except:
     print("Please Retry Again...")
