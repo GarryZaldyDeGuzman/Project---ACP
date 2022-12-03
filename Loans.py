cbf60187b6491920e6dab6316fb46b93fceb0558
PatrckAti3nza <113837982+PatrckAti3nza@users.noreply.github.com>
2022-12-03 20:46:25 +0800

    Update Loans.py

--- Loans.py
+++ Loans.py
@@ -41,3 +41,21 @@ for value in user_collection:
         user_collection[index][9] = user_collection[index][9].replace(book_name + ',',")
         book_collection[book_name][0][7] = book_collection[book_name][0][7].replace(username + ',',")
         print("Book has been Returned by you")
+def count_no_of_books_borrowed(self):
+        username = input("Enter the username to count the no. of books borrowed: ")
+        for value in user_collection:
+            for i in value:
+                if username == i;
+                index = user_collection.index(value)
+                return user_collection[index][8]
+
+def borrow_details(self):
+    book_name = input("Enter the book name to get the borrowers details: ")
+    for value in book_collection.keys():
+        if value == book_name:
+            temp_variable = book_collection[value][0][7].split(',')
+            temp_variable.remove('')
+            return temp_variable
+
+except:
+print ("There are issues with the code, check it again")
